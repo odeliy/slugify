637aa419691c4f040bdc1915005536ac54f1f378
cody iott <codyleeiott@gmail.com>
2023-08-04 11:29:39 -0700

    big fix, trailing dashes

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ let br = document.createElement('br')
 
 window.addEventListener('paste', (event) => {
 	let clipboard = event.clipboardData.getData('text/plain')
-	console.log('input: ', clipboard)
 
 	clipboard = clipboard
 		.trim()
 		.toLowerCase()
 		.replace(/[^a-z0-9+]+/gi, '-')
 
-	console.log('output:', clipboard)
+	if (clipboard.slice(-1) === '-') {
+		clipboard = clipboard.slice(0, -1)
+	}
 
 	navigator.clipboard.writeText(clipboard).then(() => {
 		resultBox.innerHTML += 'preview: ' + clipboard
